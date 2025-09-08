c1f91920ae553384f1399c34384a8d9376d103c0
Mikael2983 <mikael.picolo@laposte.net>
2025-09-08 15:49:39 +0200

    create apps profiles Move models, admin, views create a new migration to fill the new table with old data

--- oc_lettings_site/models.py
+++ oc_lettings_site/models.py
@@ -24,7 +24,10 @@ class Letting(models.Model):
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User,
+                                on_delete=models.CASCADE,
+                                related_name="oc_lettings_profile"
+                                )
     favorite_city = models.CharField(max_length=64, blank=True)
 
     def __str__(self):
